3af834a8fb9e7ca860cdcc4a2b0fff1be493b14b
jm <apolon7579@naver.com>
2021-08-27 16:30:27 +0900

    Scene Switching 실습

--- src/main/java/com/apolon7579/javafxpractice/HelloApplication.java
+++ src/main/java/com/apolon7579/javafxpractice/HelloApplication.java
@@ -6,37 +6,45 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Stack;
 
 public class HelloApplication extends Application{
 
-    Button button;
+    Stage window;
+    Scene scene1, scene2;
 
     @Override
     public void start(Stage stage) throws IOException {
+        window = stage;
 
-        stage.setTitle("Title of hte Window");
-        button = new Button();
-        button.setText("Click me");
+        Label label1 = new Label("Welcome to first scene!");
+        Button button1 = new Button("Go to scene 2");
+        button1.setOnAction(e -> window.setScene(scene2));
 
-        button.setOnAction(e -> {
-            System.out.println("람다식 연습");
-            System.out.println("여러줄도 실행가능");
-        });
+        // Layout 1 - children are laid out in vertical column
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1, button1);
+        scene1 = new Scene(layout1, 200, 200);
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+        // Button 2
+        Button button2 = new Button("Go to scene 1");
+        button2.setOnAction(e -> window.setScene(scene1));
 
-        Scene scene = new Scene(layout, 300, 250);
-        stage.setScene(scene);
-        stage.show();
+        // Layout 2
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().add(button2);
+        scene2 = new Scene(layout2, 600, 300);
 
+        window.setScene(scene1);
+        window.setTitle("Title");
+        window.show();
     }
-
-
 
     public static void main(String[] args) {
         launch();
