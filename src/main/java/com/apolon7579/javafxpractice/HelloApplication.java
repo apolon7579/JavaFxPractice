0d1b0e2542f9ff21caa92bafff4df9ea2cd6c0ff
jm <apolon7579@naver.com>
2021-08-27 12:29:50 +0900

    Basic Window 생성

--- src/main/java/com/apolon7579/javafxpractice/HelloApplication.java
+++ src/main/java/com/apolon7579/javafxpractice/HelloApplication.java
@@ -3,18 +3,30 @@ package com.apolon7579.javafxpractice;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    Button button;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        stage.setTitle("Title of hte Window");
+        button = new Button();
+        button.setText("Click me");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
