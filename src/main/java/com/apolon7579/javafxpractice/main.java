ebb8c041413137760a3bfc366a1b911855b23eda
jm <apolon7579@naver.com>
2021-08-28 05:36:53 +0900

    Creating Modal 실습

---
+++ src/main/java/com/apolon7579/javafxpractice/main.java
@@ -0,0 +1,40 @@
+package com.apolon7579.javafxpractice;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Stack;
+
+public class main extends Application{
+
+    Stage window;
+    Button button;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        window = stage;
+        window.setTitle("Joon");
+
+        button = new Button("Click me");
+        button.setOnAction(e-> AlertBox.display("Title","window modal"));
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+        Scene scene = new Scene(layout, 300, 250);
+        window.setScene(scene);
+        window.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
