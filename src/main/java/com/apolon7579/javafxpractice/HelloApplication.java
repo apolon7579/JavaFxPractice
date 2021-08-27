8d5d4eb8332a92358bf1d96bc0f335a71f8dd64a
jm <apolon7579@naver.com>
2021-08-27 14:25:48 +0900

    Handle User Events 실습

--- src/main/java/com/apolon7579/javafxpractice/HelloApplication.java
+++ src/main/java/com/apolon7579/javafxpractice/HelloApplication.java
@@ -1,6 +1,8 @@
 package com.apolon7579.javafxpractice;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -9,7 +11,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class HelloApplication extends Application implements EventHandler<ActionEvent> {
 
     Button button;
 
@@ -20,6 +22,8 @@ public class HelloApplication extends Application {
         button = new Button();
         button.setText("Click me");
 
+        button.setOnAction(this);
+
         StackPane layout = new StackPane();
         layout.getChildren().add(button);
 
@@ -27,6 +31,13 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
 
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        if(actionEvent.getSource() == button){
+            System.out.println("Button Clicked");
+        }
     }
 
     public static void main(String[] args) {
