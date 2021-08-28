efde08ea566d17971a2b005c7c846c16b3342023
jm <apolon7579@naver.com>
2021-08-28 12:15:49 +0900

    Check Box 실습

--- src/main/java/com/apolon7579/javafxpractice/main.java
+++ src/main/java/com/apolon7579/javafxpractice/main.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
@@ -27,30 +28,35 @@ public class main extends Application{
         window = primaryStage;
         window.setTitle("Joon");
 
-        //Name input
-        TextField nameInput = new TextField();
-        button = new Button("Click me");
-        button.setOnAction(e -> isInt(nameInput, nameInput.getText()) );
+        //Checkboxes
+        CheckBox box1 = new CheckBox("Bacon");
+        CheckBox box2 = new CheckBox("Tuna");
+        box2.setSelected(true);
+
+        button = new Button("Order Now!");
+        button.setOnAction(e -> handleOptions(box1, box2));
 
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20, 20, 20, 20));
-        layout.getChildren().addAll(nameInput, button);
+        layout.getChildren().addAll(box1, box2, button);
 
         scene = new Scene(layout, 300, 250);
         window.setScene(scene);
         window.show();
     }
 
-    private boolean isInt(TextField input, String message){
-        try{
-            int age = Integer.parseInt(input.getText());
-            System.out.println("User is: " + age);
-            return true;
+    private void handleOptions(CheckBox box1, CheckBox box2){
+        String message = "Users order:\n";
 
-        }catch(NumberFormatException e){
-            System.out.println("Error: " + message + " is not a number");
-            return false;
+        if(box1.isSelected()){
+            message += "Bacon";
         }
+
+        if(box2.isSelected()){
+            message += "Tuna";
+        }
+
+        System.out.println(message);
     }
 
     public static void main(String[] args) {
