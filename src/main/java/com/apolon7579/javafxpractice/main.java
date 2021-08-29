1aee02cc26da7fd2fd98a342bdf33cef00ed8493
jm <apolon7579@naver.com>
2021-08-29 15:15:23 +0900

    Simple TableView 실습

--- src/main/java/com/apolon7579/javafxpractice/main.java
+++ src/main/java/com/apolon7579/javafxpractice/main.java
@@ -1,6 +1,7 @@
 package com.apolon7579.javafxpractice;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -8,6 +9,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -18,22 +20,48 @@ import java.util.Stack;
 public class main extends Application{
 
     Stage window;
-
+    TableView<Product> table;
 
     @Override
     public void start(Stage primaryStage) throws IOException {
         window = primaryStage;
         window.setTitle("Joon");
 
+        TableColumn<Product, String> nameColumn = new TableColumn<>("Name");
+        nameColumn.setMinWidth(200);
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
 
+        TableColumn<Product, Double> priceColumn = new TableColumn<>("Price");
+        nameColumn.setMinWidth(200);
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+        TableColumn<Product, Double> quantityColumn = new TableColumn<>("Quantity");
+        nameColumn.setMinWidth(200);
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+
+        table = new TableView<>();
+        table.setItems(getProduct());
+        table.getColumns().addAll(nameColumn, priceColumn, quantityColumn);
 
         VBox layout = new VBox();
-        layout.getChildren().addAll();
+        layout.getChildren().addAll(table);
 
-        Scene scene = new Scene(layout, 300, 200);
+        Scene scene = new Scene(layout);
         window.setScene(scene);
         window.show();
     }
+
+    public ObservableList<Product> getProduct(){
+        ObservableList<Product> products = FXCollections.observableArrayList();
+        products.add(new Product("Laptop", 859.00, 20));
+        products.add(new Product("Bouncy Ball", 30, 42));
+        products.add(new Product("Toilet", 50, 11));
+        products.add(new Product("The Notebook DVD", 70, 4));
+        products.add(new Product("Corn", 10, 5));
+
+        return products;
+    }
+
     public static void main(String[] args) {
         launch();
     }
