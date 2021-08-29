439550abdea7bacef83a18333ec47f834be2bac1
jm <apolon7579@naver.com>
2021-08-29 13:44:02 +0900

    Introduction TableView 실습

--- src/main/java/com/apolon7579/javafxpractice/main.java
+++ src/main/java/com/apolon7579/javafxpractice/main.java
@@ -18,7 +18,6 @@ import java.util.Stack;
 public class main extends Application{
 
     Stage window;
-    TreeView<String> tree;
 
 
     @Override
@@ -26,42 +25,15 @@ public class main extends Application{
         window = primaryStage;
         window.setTitle("Joon");
 
-        TreeItem<String> root, bucky, megan;
 
-        root = new TreeItem<>();
-        root.setExpanded(true);
 
-        bucky = makeBranch("Bucky", root);
-        makeBranch("thenewboston", bucky);
-        makeBranch("YouTube", bucky);
-        makeBranch("Chicken", bucky);
+        VBox layout = new VBox();
+        layout.getChildren().addAll();
 
-        megan = makeBranch("Megan", root);
-        makeBranch("Glitter", megan);
-        makeBranch("Makeup", megan);
-
-        tree = new TreeView<>(root);
-        tree.setShowRoot(false);
-        tree.getSelectionModel().selectedItemProperty().addListener( (v, oldValue, newValue) -> {
-            if(newValue != null)
-                System.out.println(newValue.getValue());
-        });
-
-        StackPane layout = new StackPane();
-        layout.getChildren().add(tree);
         Scene scene = new Scene(layout, 300, 200);
         window.setScene(scene);
         window.show();
     }
-
-    public TreeItem<String> makeBranch(String title, TreeItem<String> parent){
-        TreeItem<String> item = new TreeItem<>(title);
-        item.setExpanded(true);
-        parent.getChildren().add(item);
-
-        return item;
-    }
-
     public static void main(String[] args) {
         launch();
     }
