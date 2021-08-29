184b0b92fbdc03c35b69b5d81af4223ea83268cb
jm <apolon7579@naver.com>
2021-08-30 01:44:50 +0900

    CSS Themes and Styles 실습

--- src/main/java/com/apolon7579/javafxpractice/main.java
+++ src/main/java/com/apolon7579/javafxpractice/main.java
@@ -1,92 +1,59 @@
 package com.apolon7579.javafxpractice;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Stack;
 
 public class main extends Application{
 
     Stage window;
-    BorderPane layout;
 
     @Override
     public void start(Stage primaryStage) throws IOException {
         window = primaryStage;
         window.setTitle("Joon");
 
-        Menu fileMenu = new Menu("File");
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setVgap(8);
+        grid.setHgap(10);
 
-        MenuItem newFile = new MenuItem("New....");
-        newFile.setOnAction(e -> System.out.println("Create a new file..."));
-        fileMenu.getItems().add(newFile);
+        //Name label
+        Label nameLabel = new Label("Username:");
+        GridPane.setConstraints(nameLabel, 0, 0);
 
-        fileMenu.getItems().add(new MenuItem("Open..."));
-        fileMenu.getItems().add(new MenuItem("Save..."));
-        fileMenu.getItems().add(new SeparatorMenuItem());
-        fileMenu.getItems().add(new MenuItem("Settings..."));
-        fileMenu.getItems().add(new MenuItem("Exit..."));
+        //Name input
+        TextField nameInput = new TextField("Bucky");
+        GridPane.setConstraints(nameInput, 1, 0);
 
-        Menu editMenu = new Menu("_Edit");
-        editMenu.getItems().add(new MenuItem("Cut"));
-        editMenu.getItems().add(new MenuItem("Copy"));
+        //Password label
+        Label passLabel = new Label("password:");
+        GridPane.setConstraints(nameLabel, 0, 1);
 
-        MenuItem paste = new MenuItem("Paste");
-        paste.setOnAction(e -> System.out.println("Paste some crap"));
-        paste.setDisable(true);
-        editMenu.getItems().add(paste);
+        //Pasword input
+        TextField passInput = new TextField();
+        passInput.setPromptText("password");
+        GridPane.setConstraints(passInput, 1, 1);
 
-        //Help Menu
-        Menu helpMenu = new Menu("Help");
-        CheckMenuItem showLines = new CheckMenuItem("Show Line Numbers");
-        showLines.setOnAction(e -> {
-            if(showLines.isSelected()){
-                System.out.println("Program will now display line numbers");
-            }else{
-                System.out.println("Hiding line number");
-            }
-        });
-        CheckMenuItem autoSave = new CheckMenuItem("Enable Autosave");
-        autoSave.setSelected(true);
-        helpMenu.getItems().addAll(showLines, autoSave);
+        //Login
+        Button loginButton = new Button("Log in");
+        GridPane.setConstraints(loginButton, 1, 2);
 
-        //Difficulty RadioMenuItems
-        Menu difficultyMenu = new Menu("Difficulty");
-        ToggleGroup difficultyToggle = new ToggleGroup();
+        //Add Everything to grid
+        grid.getChildren().addAll(nameLabel, nameInput, passLabel, passInput, loginButton);
 
-        RadioMenuItem easy = new RadioMenuItem("Easy");
-        RadioMenuItem medium = new RadioMenuItem("medium");
-        RadioMenuItem hard = new RadioMenuItem("hard");
-
-        easy.setToggleGroup(difficultyToggle);
-        medium.setToggleGroup(difficultyToggle);
-        hard.setToggleGroup(difficultyToggle);
-        difficultyMenu.getItems().addAll(easy, medium, hard);
-
-        // Main menu bar
-        MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().addAll(fileMenu, editMenu, helpMenu, difficultyMenu);
-
-        layout = new BorderPane();
-        layout.setTop(menuBar);
-
-        Scene scene = new Scene(layout, 300, 200);
+        Scene scene = new Scene(grid, 300, 200);
+        scene.getStylesheets().add("Viper.css");
         window.setScene(scene);
         window.show();
     }
-
 
     public static void main(String[] args) {
         launch();
