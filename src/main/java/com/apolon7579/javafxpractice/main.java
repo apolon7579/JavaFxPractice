0b553f42b4f2ac3714cc855f091adcd2f1618588
jm <apolon7579@naver.com>
2021-08-30 12:49:55 +0900

    Controllers in FXML 실습

--- src/main/java/com/apolon7579/javafxpractice/main.java
+++ src/main/java/com/apolon7579/javafxpractice/main.java
@@ -3,8 +3,10 @@ package com.apolon7579.javafxpractice;
 import javafx.application.Application;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -16,30 +18,14 @@ import java.io.IOException;
 
 public class main extends Application{
 
-    Stage window;
-
     @Override
     public void start(Stage primaryStage) throws IOException {
-        window = primaryStage;
-        window.setTitle("Joon");
 
-        //Input and labels
-        TextField userInput = new TextField();
-        userInput.setMaxWidth(200);
-        Label firstLabel = new Label("Welcome to the site");
-        Label secondLabel = new Label();
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
 
-        HBox bottomText = new HBox(firstLabel, secondLabel);
-        bottomText.setAlignment(Pos.CENTER);
-
-        VBox vBox = new VBox(10, userInput, bottomText);
-        vBox.setAlignment(Pos.CENTER);
-
-        secondLabel.textProperty().bind(userInput.textProperty());
-
-        Scene scene = new Scene(vBox, 300, 200);
-        window.setScene(scene);
-        window.show();
     }
 
     public static void main(String[] args) {
